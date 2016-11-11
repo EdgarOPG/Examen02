8326e0b32a2d3eb1363daaf7c5ecc78cb7ef35d6
edgar peinado <eopg95@gmail.com>
2016-11-10 20:06:16 -0700

    subir examen

---
+++ sql.sql
@@ -0,0 +1,3 @@
+create table computadoras(modelo varchar(20), marca varchar(20), tipo varchar(20), precio float, anio_fab date,vigente boolean);
+create table alumno(nombre varchar(20), apellifo varchar(20), apellido2 varchar(20), edad int, curp varchar(13),rfc varchar(10));
+create table pelicula(titulo varchar(20), anio date, sinopsis varchar(300), vigente boolean);
